c4ab39be12413a6451e30044ea0b1767220d60e2
Renan Salustiano <salusoftware@gmail.com>
2025-12-13 23:04:51 -0300

    feat: add Residents to dbContext

--- src/DeliveryManager.Infrastructure/Persistence/DeliveryManagerDbContext.cs
+++ src/DeliveryManager.Infrastructure/Persistence/DeliveryManagerDbContext.cs
@@ -1,4 +1,5 @@
 using DeliveryManager.Domain.Deliveries;
+using DeliveryManager.Domain.Residents;
 using Microsoft.EntityFrameworkCore;
 
 namespace DeliveryManager.Infrastructure.Persistence;
@@ -8,6 +9,7 @@ public class DeliveryManagerDbContext : DbContext
     public DeliveryManagerDbContext(DbContextOptions<DeliveryManagerDbContext> options) : base(options) {}
     
     public DbSet<Delivery>  Deliveries { get; set; }
+    public DbSet<Resident>  Residents { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
